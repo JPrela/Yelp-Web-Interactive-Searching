176060208aad0cef1e7de56e5b7df99d24cf1515
JPrela <jp4107@columbia.edu>
2020-11-13 03:02:16 +0000

    add orders page and reviews page

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ import os
   # accessible as a variable in index.html:
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, url_for
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -41,11 +41,6 @@ engine = create_engine(DATABASEURI)
 # Example of running queries in your database
 # Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
 #
-
-
-
-
-
 
 
 @app.before_request
@@ -86,10 +81,35 @@ def clogin():
     if i['user_id']==user_id:
       indicate=1
       break
+  cursor.close()
   if indicate==1:
-    return render_template('clogin.html',**{'msg': 'login success!'})
+    return redirect(url_for('orders', user_id=user_id, business_id=0))
   else:
-    return render_template('clogin.html',**{'msg': 'user id doesn\'t exist!'})
+    return render_template('clogin.html', **{'msg': 'user id doesn\'t exist!'})
+
+@app.route('/orders/<user_id>/<business_id>/')
+def orders(user_id, business_id):
+  if user_id == "0":
+    s = text("SELECT user_id, cname, order_id, odate, bname FROM customers natural join place_orders natural join orders natural join receive_orders natural join business WHERE business_id LIKE :x")
+    value = {'x': business_id}
+    cursor = g.conn.execute(s, value)
+  else:
+    s = text("SELECT user_id, cname, order_id, odate, bname FROM customers natural join place_orders natural join orders natural join receive_orders natural join business WHERE user_id LIKE :x")
+    value = {'x': user_id}
+    cursor = g.conn.execute(s, value)
+  order=[]
+  orders=[]
+  for i in cursor:
+    order.append(i['order_id'])
+    order.append(i['odate'])
+    order.append(i['user_id'])
+    order.append(i['cname'])
+    order.append(i['bname'])
+    orders.append(order)
+  cursor.close()
+  context=dict(data=orders)
+
+  return render_template('orders.html',**context)
 
 @app.route('/blogin',methods=['GET','POST'])
 def blogin():
@@ -102,115 +122,36 @@ def blogin():
     if i['business_id']==business_id:
       indicate=1
       break
+  cursor.close()
   if indicate==1:
-    return render_template('blogin.html',**{'msg': 'login success!'})
+    return redirect(url_for('orders', user_id=0, business_id=business_id))
   else:
-    return render_template('blogin.html',**{'msg': 'user id doesn\'t exist!'})
+    return render_template('blogin.html',**{'msg': 'business id doesn\'t exist!'})
 
-#
-# @app.route is a around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
+@app.route('/orders_review/<order_id>/')
+def orders_review(order_id):
+  s = text("SELECT user_id, cname, rstars, bname, rtext FROM customers natural join review_post natural join business WHERE order_id LIKE :x")
+  value = {'x': order_id}
+  cursor = g.conn.execute(s, value)
+  review = []
+  for i in cursor:
+    review.append(i['user_id'])
+    review.append(i['cname'])
+    review.append(i['bname'])
+    review.append(i['rstars'])
+    review.append(i['rtext'])
+  cursor.close()
+  context = dict(data=review)
+  return render_template('orders_review.html',**context)
+
+
+
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
   # DEBUG: this is debugging code to see what request looks like
   print(request.args)
+  return render_template("index.html")
 
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  cursor_c = g.conn.execute("SELECT user_id FROM Customers WHERE Customers.cname = 'Gabi'")
-  list_c = []
-  for i in cursor_c:
-      list_c.append(i)
-  cursor_c.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  # context = dict(data = names)
-  context = dict(data = list_c)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
-  return redirect('/')
-
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
 
 
 if __name__ == "__main__":
